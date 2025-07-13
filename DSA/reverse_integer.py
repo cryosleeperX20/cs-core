823d0772cdff8d7d9578e7868093b2621fc3203a
Prakhar Pandey <prakharp4pandey@gmail.com>
2025-07-13 23:43:57 +0530

    Add

---
+++ DSA/reverse_integer.py
@@ -0,0 +1,11 @@
+
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x_abs = abs(x)
+        reversed_x = int(str(x_abs)[::-1])
+        
+        result = sign * reversed_x
+        
+        if result < -2**31 or result > 2**31 - 1:
+            return 0
+        return result
